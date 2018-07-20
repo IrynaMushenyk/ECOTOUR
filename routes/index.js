9ed13ee6432891334266a1b03aac3e345f0c0b75
Iryna Mushenyk <mushenik77@ukr.net>
2018-07-20 17:28:58 +0300

    Розмітка сайта

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Головна' });
+});
+router.get('/countries', function(req, res, next) {
+    res.render('countries', { title: 'Країни' });
+});
+router.get('/tours', function(req, res, next) {
+    res.render('tours', { title: 'Тури' });
+});
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'Контакти' });
+});
+module.exports = router;
